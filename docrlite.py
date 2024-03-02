4f178ce9f29c9fc0d8eddc0212a44e6ecee8f7e6
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2024-03-02 20:09:42 +0900

    Update docrlite.py

--- docrlite.py
+++ docrlite.py
@@ -1,7 +1,7 @@
 #OoO;;
-from sys import argv as a;o=open
+from sys import argv as a;o,ints=open,(lambda x:x if x==' ' else int(x))
 def f(c):
-    l=c.split('\n');s=l.pop(0)[2];l=list(map((lambda x:int(x[0])+'|'+'|'.join(x[1:]).replace(s,' ')), l.split('|')));r,n='',1
+    l=c.split('\n');s=l.pop(0)[2];l=list(map((lambda x:ints(x[0])+'|'+'|'.join(x[1:]).replace(s,' ')), l.split('|')));r,n='',1
     for i,j in l:
         if n!=i:n,r=i,r+'\n'
         r+=j
